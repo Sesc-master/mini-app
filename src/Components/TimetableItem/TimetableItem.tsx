c99bfe7461c0ee5acc59ecb5566a8674003bbbcf
fedechka <74444888+fede4ka1245@users.noreply.github.com>
2021-12-12 15:58:57 +0500

    made some typechecking

--- src/Components/TimetableItem/TimetableItem.tsx
+++ src/Components/TimetableItem/TimetableItem.tsx
@@ -11,9 +11,24 @@ type ITimetableItem = {
 }
 
 const TimetableItem = ({time, schedule}: ITimetableItem) => {
-	if (schedule.length === 1){
+	if (schedule[0] === undefined){
+		return (
+		<div className={styles.task} id='ripple'>
+			<div className={styles.date}>
+				<Text weight="semibold">
+					{time[0]}
+				</Text>
+				<Text weight="semibold">
+					{time[1]}	
+				</Text>
+			</div>
+			<div className={styles.stick}></div>
+			<div style={{paddingLeft: '5px', overflowX: 'hidden'}}>
+				<Text weight="semibold">-</Text>
+			</div>
+		</div>)
+	}else if (schedule[0].subgroup === 0){
 		let lesson = schedule[0]
-		console.log(lesson.subgroup)
 		return (
 			<div className={styles.task} id='ripple'>
 				<div className={styles.date}>
@@ -31,9 +46,50 @@ const TimetableItem = ({time, schedule}: ITimetableItem) => {
 				</div>
 			</div>
 		)
-	}else if (schedule.length === 2){
+	}else if (schedule[0].subgroup === 0){
 		let lesson = schedule[0]
-		let lesson2 = schedule[1]
+		return (
+			<div className={styles.task} id='ripple'>
+				<div className={styles.date}>
+					<Text weight="semibold" >
+						{time[0]}
+					</Text>
+					<Text weight="semibold" >
+						{time[1]}	
+					</Text>
+				</div>
+				<div className={styles.stick}></div>
+				<div style={{paddingLeft: '5px', overflowX: 'hidden'}}>
+					<Text weight="semibold">{lesson?.subject}</Text>
+					<Text weight="regular">{lesson?.teacher.split(' ')[0]} {lesson.auditory}</Text>
+				</div>
+			</div>
+		)
+	}else if (schedule.length == 2){
+		let lesson = schedule[0]
+		return (
+				<div className={styles.task} id='ripple'>
+					<div className={styles.date}>
+						<Text weight="semibold">
+							{time[0]}
+						</Text>
+						<Text weight="semibold">
+							{time[1]}	
+						</Text>
+					</div>
+					<div className={styles.stick}></div>
+					<div style={{paddingLeft: '5px', overflowX: 'hidden', width: '37%'}}>
+						<Text weight="semibold">-</Text>
+					</div>
+					<div className={styles.stick}></div>
+					<div style={{paddingLeft: '5px', overflowX: 'hidden', width: '40%'}}>
+					<Text weight="semibold">{lesson.subject}</Text>
+						<Text weight="regular">{lesson.teacher.split(' ')[0]} {lesson.auditory}</Text>
+					</div>
+				</div>
+		)
+	}else if (schedule[0].subgroup === 1){
+		let lesson = schedule[0]
 		return (
 				<div className={styles.task} id='ripple'>
 					<div className={styles.date}>
@@ -51,28 +107,50 @@ const TimetableItem = ({time, schedule}: ITimetableItem) => {
 					</div>
 					<div className={styles.stick}></div>
 					<div style={{paddingLeft: '5px', overflowX: 'hidden', width: '40%'}}>
-						<Text weight="semibold">{lesson2.subject}</Text>
-						<Text weight="regular">{lesson2.teacher.split(' ')[0]} {lesson2.auditory}</Text>
+					<Text weight="semibold">-</Text>
 					</div>
-
 				</div>
 		)
-	} else {
-		return(	
-		<div className={styles.task} id='ripple'>
-			<div className={styles.date}>
-				<Text weight="semibold">
-					{time[0]}
-				</Text>
-				<Text weight="semibold">
-					{time[1]}	
-				</Text>
+	} else if (schedule[0].subgroup === 2){
+
+		let lesson2 = schedule[0]
+		return (
+				<div className={styles.task} id='ripple'>
+					<div className={styles.date}>
+						<Text weight="semibold">
+							{time[0]}
+						</Text>
+						<Text weight="semibold">
+							{time[1]}	
+						</Text>
+					</div>
+					<div className={styles.stick}></div>
+					<div style={{paddingLeft: '5px', overflowX: 'hidden', width: '37%'}}>
+						<Text weight="semibold">-</Text>
+					</div>
+					<div className={styles.stick}></div>
+					<div style={{paddingLeft: '5px', overflowX: 'hidden', width: '40%'}}>
+						<Text weight="semibold">{lesson2?.subject}</Text>
+						<Text weight="regular">{lesson2?.teacher.split(' ')[0]} {lesson2?.auditory || '-'}</Text>
+					</div>
+				</div>
+		)
+	}else {
+		return (
+			<div className={styles.task} id='ripple'>
+				<div className={styles.date}>
+					<Text weight="semibold" >
+						{time[0]}
+					</Text>
+					<Text weight="semibold" >
+						{time[1]}	
+					</Text>
+				</div>
+				<div className={styles.stick}></div>
+				<div style={{paddingLeft: '5px', overflowX: 'hidden'}}>
+					<Text weight="semibold">-</Text>
+				</div>
 			</div>
-			<div className={styles.stick}></div>
-			<div style={{paddingLeft: '5px', overflowX: 'hidden'}}>
-				<Text weight="semibold">-</Text>
-			</div>
-		</div>
 		)
 	}
 }; 
