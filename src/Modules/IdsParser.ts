a1c18bc6725eced2cc115a99f39ca0ba1ebf5cec
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2022-01-09 22:54:44 +0500

    Refactored IdsParser and used in Table.ts

--- src/Modules/IdsParser.ts
+++ src/Modules/IdsParser.ts
@@ -1,65 +1,70 @@
 import request from "./ProxyModule";
 
-export default class IDs {
-    static server: string = "lyceum.urfu.ru";
-    static page: string = "ucheba/raspisanie-zanjatii"
+const server: string = "lyceum.urfu.ru";
+const page: string = "ucheba/raspisanie-zanjatii"
 
-    static groups: Map<string, number>;
-    static teachers: Map<string, number>;
-    static auditories: Map<string, number>;
-    static weekdays: Map<string, number>;
+const ignoringText = [
+    "Нет", "Учитель", "Выберите класс", "Выберите аудиторию", "Выберите преподавателя", "Выберите день"
+];
 
-    static ignoringText = [
-        "Нет", "Учитель", "Выберите класс", "Выберите аудиторию", "Выберите преподавателя", "Выберите день"
-    ];
+export type ParsedIDs = {
+    groups: Map<string, number>;
+    teachers: Map<string, number>;
+    auditories: Map<string, number>;
+    weekdays: Map<string, number>;
+}
 
-    static async refresh() {
-        this.groups = new Map<string, number>();
-        this.teachers = new Map<string, number>();
-        this.auditories = new Map<string, number>();
-        this.weekdays = new Map<string, number>();
-        
-        return new Promise((resolve, reject) => {
-            let pageURL = new URL(`https://${this.server}/${this.page}`);
-            return request(pageURL)
-                .then(response => {
-                    return response.text();
-                })
-                .then(stringHTMLPage => {
-                    let parser = new DOMParser();
-                    let doc = parser.parseFromString(stringHTMLPage, "text/html");
-                    let formElement = doc.querySelector("form");
-                    if (!formElement) reject();
+export default async function getIDs(): Promise<ParsedIDs> {
+    let groups = new Map<string, number>();
+    let teachers = new Map<string, number>();
+    let auditories = new Map<string, number>();
+    let weekdays = new Map<string, number>();
 
-                    let selectors = formElement?.querySelectorAll("select");
-                    if (selectors === undefined) reject();
+    return new Promise((resolve, reject) => {
+        let pageURL = new URL(`https://${server}/${page}`);
+        return request(pageURL)
+            .then(response => {
+                return response.text();
+            })
+            .then(stringHTMLPage => {
+                let parser = new DOMParser();
+                let doc = parser.parseFromString(stringHTMLPage, "text/html");
+                let formElement = doc.querySelector("form");
+                if (!formElement) reject();
+
+                let selectors = formElement?.querySelectorAll("select");
+                if (selectors === undefined) reject();
 
                 selectors?.forEach(selectElement => {
                     let selectType: Map<string, number> | undefined;
-                    switch(selectElement.getAttribute("data-name")) {
+                    switch (selectElement.getAttribute("data-name")) {
                         case "group":
-                            selectType = this.groups;
+                            selectType = groups;
                             break;
                         case "auditory":
-                            selectType = this.auditories;
+                            selectType = auditories;
                             break;
                         case "teacher":
-                            selectType = this.teachers;
+                            selectType = teachers;
                             break;
                         case "weekday":
-                            selectType = this.weekdays;
+                            selectType = weekdays;
                             break
                     }
                     for (let selectElementIndex = 0; selectElementIndex < selectElement.options.length &&
                         selectType !== undefined; selectElementIndex++) {
-                        if (!this.ignoringText.includes(selectElement.options[selectElementIndex].text))
-                            selectType.set(selectElement.options[selectElementIndex].text,
+                        if (!ignoringText.includes(selectElement.options[selectElementIndex].text)) {
+                            selectType.set(
+                                selectElement.options[selectElementIndex].text,
                                 Number(selectElement.options[selectElementIndex].value)
                             );
+                        }
                     }
                 });
-                resolve(this);
+                resolve({
+                    groups, teachers, auditories, weekdays
                 });
-        });
-    }
+            });
+    });
 }
+
--- src/Modules/Table.ts
+++ src/Modules/Table.ts
@@ -1,59 +1,25 @@
-import { getSchedule, getFullSchedule} from "./Schedule";
-import { IdableScheduleType } from "./Schedule/Schedule";
-
+import getIDs from "./IdsParser";
+import { getSchedule, getFullSchedule } from "./Schedule";
+import { IdableScheduleType, Schedule } from "./Schedule/Schedule";
 
 export class Table {
     static teachers: any;
     static auditories: any;
-	
-    public static getTable(scheduleType: IdableScheduleType, weekday: number, grade: string) {
-        switch (scheduleType) {
-            case "group":
-                return getSchedule(scheduleType, weekday, Number(Table.grades.get(grade)));
-            case "teacher":
-                // TODO: парсить учителей и их id
-                return getSchedule(scheduleType, weekday, Number(Table.grades.get(grade)));
-            case "auditory":
-                // TODO: парсить кабинеты и их id
-                return getSchedule(scheduleType, weekday, Number(Table.grades.get(grade)));
-        }
-        // TODO: случай поеботы с нeкорректным типом, послать исключением
+
+    public static async getTable(scheduleType: IdableScheduleType, weekday: number, grade: string): Promise<Schedule> {
+        return getIDs().then((IDs) => {
+            switch (scheduleType) {
+                case "group":
+                    return getSchedule(scheduleType, weekday, Number(IDs.groups.get(grade)));
+                case "teacher":
+                    return getSchedule(scheduleType, weekday, Number(IDs.teachers.get(grade)));
+                case "auditory":
+                    return getSchedule(scheduleType, weekday, Number(IDs.auditories.get(grade)));
+            }
+        });
     }
 
     public static getFullTable(weekday: number) {
         return getFullSchedule(weekday)
     }
-
-    private static grades: Map<string, number> = new Map([
-        ["8А",1],
-        ["8В",2],
-        ["9А",4],
-        ["9Б",5],
-        ["9В",3],
-        ["9Г",11],
-        ["9Е",9],
-        ["10А",12],
-        ["10Б",13],
-        ["10В",14],
-        ["10Г",15],
-        ["10Д",16],
-        ["10Е",17],
-        ["10З",18],
-        ["10К",19],
-        ["10Л",20],
-        ["10М",21],
-        ["10Н",22],
-        ["10С",23],
-        ["11А",6],
-        ["11Б",7],
-        ["11В",8],
-        ["11Г",24],
-        ["11Д",25],
-        ["11Е",26],
-        ["11З",27],
-        ["11К",28],
-        ["11Л",29],
-        ["11М",30],
-        ["11Н",32],
-        ["11С",31]])
 }
