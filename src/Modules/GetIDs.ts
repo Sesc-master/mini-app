9f7c72aea460f9eafebcf0c61eb4a7d1a91a81f5
Lukramancer <57191063+Lukramancer@users.noreply.github.com>
2022-01-28 18:35:29 +0500

    Used getIDs api method

--- src/Modules/GetIDs.ts
+++ src/Modules/GetIDs.ts
@@ -1,11 +1,4 @@
-import request from "./ProxyModule";
-
-const server: string = "lyceum.urfu.ru";
-const page: string = "ucheba/raspisanie-zanjatii"
-
-const ignoringText = [
-    "Нет", "Учитель", "Выберите класс", "Выберите аудиторию", "Выберите преподавателя", "Выберите день"
-];
+import reviver from "../APIModules/JSONReviver"
 
 export type ParsedIDs = {
     groups: Map<string, number>;
@@ -15,74 +8,7 @@ export type ParsedIDs = {
 }
 
 export default async function getIDs(): Promise<ParsedIDs> {
-    let groups = new Map<string, number>();
-    let teachers = new Map<string, number>();
-    let auditories = new Map<string, number>();
-    let weekdays = new Map<string, number>();
-
-    if (sessionStorage.getItem('ids') !== null){
-        const IDs = JSON.parse(sessionStorage.getItem('ids') || '{}')
-        return new Promise( (resolve, reject) => {
-            resolve({
-                groups: new Map(Object.entries(IDs.groups)),
-                teachers: new Map(Object.entries(IDs.teachers)),
-                auditories: new Map(Object.entries(IDs.auditories)),
-                weekdays: new Map(Object.entries(IDs.weekdays))
-            })
-        })
-    }
-
-    return new Promise((resolve, reject) => {
-        let pageURL = new URL(`https://${server}/${page}`);
-        return request(pageURL)
-            .then(response => {
-                return response.text();
-            })
-            .then(stringHTMLPage => {
-                let parser = new DOMParser();
-                let doc = parser.parseFromString(stringHTMLPage, "text/html");
-                let formElement = doc.querySelector("form");
-                if (!formElement) reject();
-
-                let selectors = formElement?.querySelectorAll("select");
-                if (selectors === undefined) reject();
-
-                selectors?.forEach(selectElement => {
-                    let selectType: Map<string, number> | undefined;
-                    switch (selectElement.getAttribute("data-name")) {
-                        case "group":
-                            selectType = groups;
-                            break;
-                        case "auditory":
-                            selectType = auditories;
-                            break;
-                        case "teacher":
-                            selectType = teachers;
-                            break;
-                        case "weekday":
-                            selectType = weekdays;
-                            break
-                    }
-                    for (let selectElementIndex = 0; selectElementIndex < selectElement.options.length &&
-                    selectType !== undefined; selectElementIndex++) {
-                        if (!ignoringText.includes(selectElement.options[selectElementIndex].text)) {
-                            selectType.set(
-                                selectElement.options[selectElementIndex].text,
-                                Number(selectElement.options[selectElementIndex].value)
-                            );
-                        }
-                    }
-                });
-                sessionStorage.setItem('ids', JSON.stringify({
-                    groups: Object.fromEntries(groups),
-                    teachers: Object.fromEntries(teachers),
-                    auditories: Object.fromEntries(auditories),
-                    weekdays: Object.fromEntries(weekdays)
-                }))
-                resolve({
-                    groups, teachers, auditories, weekdays
-                });
-            });
-    });
+    return fetch("/api/sesc/getIDs").then(response => response.text()).then(text => 
+        JSON.parse(text, reviver([["groups", "teachers", "auditories", "weekdays"]]))
+    );
 }
-
