ca4f203fc45e4e94ea0f55cbf786afa5f5d847a0
fedechka1245 <fedechka06@gmail.com>
2022-04-02 12:50:28 +0500

    create Informer

---
+++ frontend/src/components/informer/Informer.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import {Event, IEvent} from "../../modules/event/Event";
+import styles from "./Informer.module.scss";
+
+const Informer = () => {
+    const [event, setEvent] = useState<IEvent | undefined>();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setEvent(new Event().getCurrentEvent())
+        }, 250)
+        return () => clearInterval(interval);
+    }, [])
+
+    const formatTime = (sec: number) => {
+        const minutes = Math.floor(sec / 60) >= 10 ?
+            Math.floor(sec / 60) : "0" + Math.floor(sec / 60);
+        const seconds = Math.floor(sec % 60) >= 10 ?
+            Math.floor(sec % 60) : "0" + Math.floor(sec % 60);
+
+        return `${minutes}:${seconds}`;
+    }
+
+    return (
+        <div className={styles.content}>
+            {event && <div className={styles.text}>
+                {event?.order + 1} {event?.type}, {formatTime(Number(event?.timeToEnd))}
+            </div>}
+            <div className={styles.bar}>
+                {event && <div style={{width: `${event?.percent}%`}} className={styles.timer}/>}
+            </div>
+        </div>
+    );
+};
+
+export default Informer;
