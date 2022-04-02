424c984bc6712c8ee4c2e5f957028cb8158377c1
fedechka1245 <fedechka06@gmail.com>
2022-04-02 12:49:49 +0500

    fix timetable look

--- frontend/src/pages/timetable/Timetable.tsx
+++ frontend/src/pages/timetable/Timetable.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Div, FormItem, SelectMimicry} from "@vkontakte/vkui";
 import Week from "./week/Week";
 import {listifySchedule} from "../../modules/ListifySchedule";
@@ -13,6 +13,7 @@ import Instruction from "./Instruction";
 import ScheduleLoader from "./ScheduleLoader";
 import Error from "./error/Error";
 import Schedule from "./schedule/Schedule";
+import Informer from "../../components/informer/Informer";
 
 const Timetable = () => {
     const {grade, weekSchedule, isTimetableLoading, isTeacher, teacher, day} = useStore(timetableStore);
@@ -23,15 +24,14 @@ const Timetable = () => {
     }, []);
 
     return (
-        <div>
-            <FormItem top={"Выберите класс или учителя"}>
-                <SelectMimicry
+        <div className='elements'>
+            <Informer/>
+            <SelectMimicry
                     placeholder="Не выбран"
                     onClick={() => setModalView(Modal.Type)}
-                >{isTeacher ? teacher : grade}</SelectMimicry>
-            </FormItem>
+            >{isTeacher ? teacher : grade}</SelectMimicry>
             <Week />
-            <div className='elements'>
+            <div>
                 {isTeacher && teacher === "" || !isTeacher && grade === "" ? (
                     <Instruction />
                 ) : isTimetableLoading ? (
