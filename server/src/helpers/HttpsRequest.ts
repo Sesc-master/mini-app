11c37d8341cf90ce251dc8832cac60be86863495
fedechka1245 <bragin.fedor96@gmail.com>
2022-04-08 10:51:33 +0500

    add axios

--- server/src/helpers/HttpsRequest.ts
+++ server/src/helpers/HttpsRequest.ts
@@ -1,29 +1,5 @@
-import { IncomingMessage } from "http";
-import { request, RequestOptions } from "https";
+import axios from "axios";
 
-export default async function httpsRequest(options: RequestOptions, payload?: string): Promise<{message: IncomingMessage, body: string}> {
-    return new Promise((resolve, reject) => {
-        let scoleRequest = request(options, response => {
-            let body = Buffer.alloc(0);
-
-            response.on("data", (chunk: Buffer) => {
-                body = Buffer.concat([body, chunk]);
-            });
-            response.on("end", () => {
-                resolve({
-                    message: response,
-                    body: body.toString()
-                });
-            });
-
-            response.on("error", (error) => {
-                reject(error);
-            });
-        });
-
-        scoleRequest.on("timeout", () => reject("timeout"));
-
-        if (payload) scoleRequest.write(payload);
-        scoleRequest.end();
-    });
+export default async function httpsRequest(options: any): Promise<any> {
+    return axios(options);
 }
