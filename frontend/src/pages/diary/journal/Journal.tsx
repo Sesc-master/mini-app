d7c48b0e9b765a5f56b35540d16ed2e78cac4e57
fedechka1245 <bragin.fedor96@gmail.com>
2022-04-10 13:48:46 +0500

    add sorting in diary

--- frontend/src/pages/diary/journal/Journal.tsx
+++ frontend/src/pages/diary/journal/Journal.tsx
@@ -1,17 +1,37 @@
-import React from "react";
-import {FormItem, SelectMimicry, Div} from "@vkontakte/vkui";
+import React, {useState, useMemo, useEffect, useCallback} from "react";
+import {FormItem, SelectMimicry, SegmentedControl} from "@vkontakte/vkui";
 import Task from "./task/Task";
 import {Modal} from "../../../modules/Modal";
 import {setModalView} from "../../../modules/effector/AppSettingsSrore";
-import {diaryStore} from "../../../modules/effector/DiaryStore";
+import {diaryStore, setDiary} from "../../../modules/effector/DiaryStore";
 import {useStore} from "effector-react"
 import styles from "./Journal.module.scss"
+import dateComparator from "../../../modules/scoleAPI/date/dateComparator";
+
+enum ISortingType {
+    NewerToOlder = 1,
+    OlderToNewer = 2
+}
 
 const Journal = () => {
     const {diary, targetSubject} = useStore(diaryStore)
     const teacher = diary.get(targetSubject)?.teacher
-    const notes = diary.get(targetSubject)?.notes
+    const tasks = diary.get(targetSubject)?.notes
+    const [sortingType, setSortingType] = useState<ISortingType>(ISortingType.NewerToOlder);
 
+    useEffect(() => {
+        if (!tasks) return;
+
+        const newDiary = diary;
+        newDiary.get(targetSubject).notes = Array.from(tasks).sort((a : any, b : any) => {
+            if (sortingType === ISortingType.OlderToNewer){
+                return dateComparator(a.date, b.date)
+            } else {
+                return -dateComparator(a.date, b.date)
+            }
+        })
+        setDiary(newDiary)
+    }, [sortingType, targetSubject])
 
     return (
         <div className="content">
@@ -19,9 +39,23 @@ const Journal = () => {
                 placeholder="предмет не выбран"
                 onClick={() => setModalView(Modal.Subjects)}
             >{!!targetSubject && `${targetSubject}. ${teacher}`}</SelectMimicry>
-            {notes !== undefined && [...notes]?.map((note, index) => 
+            {tasks && <SegmentedControl
+                options={[
+                    {
+                        label: "От новых к старым",
+                        value: ISortingType.NewerToOlder,
+                    },
+                    {
+                        label: "От старых к новым",
+                        value: ISortingType.OlderToNewer,
+                    }
+                ]}
+                onChange={(value ) => setSortingType(value as ISortingType)}
+                defaultValue={sortingType}
+            />}
+            {tasks && Array.from(tasks)?.map((task: any, index) =>
             (<div className={styles.task} key={index}>
-                <Task date={note.date} topic={note.theme} homework={note.hometask} weight={note.coefficient} mark={note.grades}/>
+                <Task date={task.date} topic={task.theme} homework={task.hometask} weight={task.coefficient} mark={task.grades}/>
             </div>
             ))}
         </div>
