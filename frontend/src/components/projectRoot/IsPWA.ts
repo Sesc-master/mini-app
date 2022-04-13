eeb3f810fd6e95b0bcf527c5f5dfc90296e06f46
fedechka1245 <bragin.fedor96@gmail.com>
2022-04-13 09:19:56 +0500

    fix isPWA

--- frontend/src/components/projectRoot/IsPWA.ts
+++ frontend/src/components/projectRoot/IsPWA.ts
@@ -1,3 +1,4 @@
 export function isPWA() {
-    return window.matchMedia("(display-mode: standalone)").matches;
+    return window.matchMedia("(display-mode: standalone)").matches ||
+        window.matchMedia("(display-mode: minimal-ui)").matches;
 }
