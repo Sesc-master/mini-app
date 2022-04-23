131e992dc9efc1a2b775b20628c8dc3a1378826c
fedechka1245 <bragin.fedor96@gmail.com>
2022-04-23 05:26:08 +0500

    catch timeout error

--- server/src/helpers/SESCRequest.ts
+++ server/src/helpers/SESCRequest.ts
@@ -3,10 +3,20 @@ import {IncomingMessage} from "http";
 import httpsRequest from "./HttpsRequest";
 
 export default async function SESCRequest(options: RequestOptions, payload?: string): Promise<{message: IncomingMessage, body: string}> {
-    let response = await httpsRequest(options, payload);
-    while (response.body.includes("Page is being generated.")) {
-        await new Promise((resolve) => setTimeout(resolve, 7000));
+    let response;
+    try {
         response = await httpsRequest(options, payload);
+        while (response.body.includes("Page is being generated.")) {
+            await new Promise((resolve) => setTimeout(resolve, 7000));
+            response = await httpsRequest(options, payload);
+        }
+        return response;
+    } catch (error: any){
+        if (error === "timeout"){
+            response = await httpsRequest(options, payload);
+            return response;
+        }
+
+        throw new Error(error);
     }
-    return response;
 }
