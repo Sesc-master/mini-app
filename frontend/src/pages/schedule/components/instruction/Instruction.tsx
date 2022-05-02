1773407e2e65718fdded20b2f1bc6bfd0c0127de
fedechka1245 <fedechka06@gmail.com>
2022-05-02 11:55:28 +0500

    fix schedule's Instruction
    *fix font

--- frontend/src/pages/schedule/components/instruction/Instruction.tsx
+++ frontend/src/pages/schedule/components/instruction/Instruction.tsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography";
 const Instruction = () => {
     return (
         <div style={{margin: "2.5em"}}>
-            <Typography align={"center"} variant={"subtitle1"}>Выберите класс или учителя</Typography>
+            <Typography align={"center"} variant={"h5"}>Выберите класс или учителя</Typography>
         </div>
     );
 };
