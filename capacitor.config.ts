840ea4411848b261480c4c9853f0b34c28023091
fedechka1245 <fedechka06@gmail.com>
2022-05-06 01:21:31 +0500

    add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sescmaster.app',
+  appName: 'sescmaster',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
